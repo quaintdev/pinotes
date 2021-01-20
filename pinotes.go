13f068afe876e6583e30c59b6324a3504494be67
Rohan Deshmukh <rohanrd@outlook.in>
2021-01-21 00:51:19 +0530

    Added default extension Changed default notes file Refactored code

--- pinotes.go
+++ pinotes.go
@@ -12,18 +12,21 @@ import (
 )
 
 //Conf configuration for the note service
+var config Conf
+
 type Conf struct {
-	Path         string
-	WebNotesFile string
-	Port         string
-	InterfaceIP  string
-	ViewNotes    bool
+	Path             string
+	DefaultNotesFile string
+	DefaultExtension string
+	Port             string
+	InterfaceIP      string
+	CanViewNotes     bool
 }
 
 //Note holds filename of the note and its content
 type Note struct {
-	FileName string
-	Content  string
+	Topic   string
+	Content string
 }
 
 func main() {
@@ -32,7 +35,6 @@ func main() {
 		log.Printf("config not present. err: %s exiting.", err)
 		return
 	}
-	var config Conf
 	err = json.Unmarshal(configBytes, &config)
 	if err != nil {
 		log.Println("invalid config present. err", err)
@@ -41,16 +43,16 @@ func main() {
 
 	fs := http.FileServer(http.Dir(config.Path))
 
-	http.HandleFunc("/add", handleAdd(&config))
-	http.Handle("/", setHeadersAndServe(&config, fs))
+	http.HandleFunc("/add", handleAdd)
+	http.Handle("/", setHeadersAndServe(fs))
 
 	log.Println("Starting server on", config.InterfaceIP+":"+config.Port)
 	http.ListenAndServe(config.InterfaceIP+":"+config.Port, nil)
 }
 
-func setHeadersAndServe(c *Conf, f http.Handler) http.HandlerFunc {
+func setHeadersAndServe(f http.Handler) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		if !c.ViewNotes {
+		if !config.CanViewNotes {
 			writer.WriteHeader(http.StatusNotFound)
 			return
 		}
@@ -60,61 +62,70 @@ func setHeadersAndServe(c *Conf, f http.Handler) http.HandlerFunc {
 	}
 }
 
-func handleAdd(config *Conf) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var n Note
-		switch r.Method {
-		case "POST":
-			var postReq map[string]interface{}
-			err := json.NewDecoder(r.Body).Decode(&postReq)
-			if err != nil {
-				log.Println("error decoding request")
-			}
-			n.FileName = postReq["title"].(string)
-			n.Content = postReq["content"].(string)
-			if !n.Save(config) {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			w.WriteHeader(http.StatusOK)
-		case "GET":
-			query := r.URL.Query().Get("q")
-			log.Println("query received", query)
-			if len(query) < 2 {
-				log.Println("expecting query parameter")
-				return
-			}
-			querySlice := strings.Split(query, "!")
-			if len(querySlice) == 1 {
-				n.FileName = config.WebNotesFile
-				n.Content = querySlice[0]
-			} else {
-				n.FileName = querySlice[0]
-				n.Content = querySlice[1]
-			}
-			//special cases that can be auto formatted in markdown
-			switch n.FileName {
-			case "quotes":
-				n.Content = "% " + n.Content
-			case "todo":
-				n.Content = "1. " + n.Content
-			}
-			n.Save(config)
-			content, err := n.Read(config)
-			if err != nil {
-				log.Println("error reading note", n.FileName)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Write(content)
-		default:
-			fmt.Println("method not supported")
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	var n Note
+	switch r.Method {
+	case "POST":
+		var postReq map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&postReq)
+		if err != nil {
+			log.Println("error decoding request")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+		n.Topic = postReq["title"].(string)
+		n.Content = postReq["content"].(string)
+		if !n.Save() {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	case "GET":
+		query := r.URL.Query().Get("q")
+		log.Println("query received", query)
+		if len(query) < 2 {
+			log.Println("expecting query parameter")
+			return
+		}
+		topicWithContent := strings.Split(query, "!")
+		if len(query) == 1 {
+			n.Topic = config.DefaultNotesFile
+			n.Content = topicWithContent[0]
+		} else {
+			n.Topic = topicWithContent[0]
+			n.Content = topicWithContent[1]
+		}
+		n.Process()
+		n.Save()
+		content, err := n.Read()
+		if err != nil {
+			log.Println("error reading note", n.Topic)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(content)
+	default:
+		fmt.Println("method not supported")
+	}
+}
+
+func (n *Note) Process() {
+	switch n.Topic {
+	case "quotes":
+		n.Topic = n.Topic + ".txt"
+		n.Content = "% " + n.Content
+	case "todo":
+		n.Content = "1. " + n.Content
 	}
 }
 
 //Save saves the note to location specified in config
-func (n *Note) Save(config *Conf) bool {
-	noteFilePath := path.Join(config.Path, n.FileName+".md")
+func (n *Note) Save() bool {
+	fileName := n.Topic
+	if !strings.Contains(fileName, ".") {
+		fileName = fileName + config.DefaultExtension
+	}
+	noteFilePath := path.Join(config.Path, fileName)
 	f, err := os.OpenFile(noteFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Printf("can't open %s, err: %s", noteFilePath, err)
@@ -129,8 +140,12 @@ func (n *Note) Save(config *Conf) bool {
 	return true
 }
 
-func (n *Note) Read(config *Conf) ([]byte, error) {
-	noteFilePath := path.Join(config.Path, n.FileName+".md")
+func (n *Note) Read() ([]byte, error) {
+	fileName := n.Topic
+	if !strings.Contains(fileName, ".") {
+		fileName = fileName + config.DefaultExtension
+	}
+	noteFilePath := path.Join(config.Path, fileName)
 	notesContent, err := ioutil.ReadFile(noteFilePath)
 	if err != nil {
 		return notesContent, fmt.Errorf("can't read file. err: %s", err)
